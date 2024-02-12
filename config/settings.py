ce9bdf6ea839021680badc3977790a7ac8b2bede
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2024-02-12 22:58:50 +0300

    created File model

--- config/settings.py
+++ config/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
     'django.contrib.staticfiles',
 
     'rest_framework',
@@ -44,9 +46,11 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
@@ -118,9 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'data/')
 MEDIA_URL = '/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- file/models.py
+++ file/models.py
@@ -1,2 +1,27 @@
 from django.db import models
 
+
+class File(models.Model):
+    """
+    Модель для описания файлов
+    """
+    file = models.FileField(
+        upload_to='files/', unique=True, verbose_name='Файл'
+    )
+    uploaded_at = models.DateTimeField(
+        auto_now_add=True, verbose_name='Дата загрузки'
+    )
+    processed = models.BooleanField(
+        default=False, verbose_name='Статус обработки'
+    )
+
+    class Meta:
+        verbose_name = 'File'
+        verbose_name_plural = 'Files'
+        db_table = 'file'
+
+    def __str__(self):
+        return f'{self.file}: {self.uploaded_at}'
+
+    def __repr__(self):
+        return f'File({self.file}, {self.uploaded_at}, {self.processed})'
