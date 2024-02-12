4f85c6514b535b0252e0f31b22df283c0952ce1c
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2024-02-12 23:47:56 +0300

    created controllers

--- file/views.py
+++ file/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework import status
+from rest_framework.parsers import FormParser, MultiPartParser
+from rest_framework.response import Response
+from file.models import File
+from file.serializers import FileListSerializer, FileUploadSerializer
 
-# Create your views here.
+
+class FileUploadView(CreateAPIView):
+    """Контроллер для загрузки файлов"""
+    queryset = File.objects.all()
+    serializer_class = FileUploadSerializer
+    parser_classes = (MultiPartParser, FormParser)
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class FileListView(ListAPIView):
+    """Контроллер для получения списка файлов"""
+    queryset = File.objects.all()
+    serializer_class = FileListSerializer
